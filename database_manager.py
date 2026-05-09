6fe348749427f28856905eb028437f9b75e6557a
Mihir Borkar (mihirb) <mihirb@complab.uci.edu>
2026-05-09 16:59:23 -0700

    get merchant id

--- database_manager.py
+++ database_manager.py
@@ -1,17 +1,51 @@
 import sqlite3
 
-def save_transaction(transaction):  
-    # takes in a transaction object (validated) from app.py, inserts its data into SQL
+def save_transaction(transaction: obj) -> None:  
+    '''
+    Takes in a transaction object (validated) from app.py, inserts its data into SQL
+    '''
     with sqlite3.connect("visa_ledger.db") as conn:
         cursor = conn.cursor()
 
         cursor.execute('''
-            INSERT INTO transactions(amount, merchant, country, rate, status)
+            INSERT INTO transactions(amount, merchant_id, country, rate, status)
             VALUES (?, ?, ?, ?, ?)
-        ''', (transaction.amount, transaction.merchant,
+        ''', (transaction.amount, transaction.merchant_id,
             getattr(transaction, "country", "USA"),
             getattr(transaction, "rate", 1.0),
             transaction.status)
         )
 
         conn.commit()
+
+def get_or_create_merchant(merchant: str, category: str="General", risk_level: float=0.1) -> int:
+    '''
+    Searches for merchant name in 'merchants' table in database.
+    If name does not exist, create a new row for that specific merchant
+    Return ID of merchant row regardless.
+    '''
+    with sqlite3.connect("visa_ledger.db") as conn:
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            SELECT id
+            FROM merchants
+            WHERE name = ?
+        ''', (merchant,))
+
+        id = cursor.fetchone() # returns result from select, which is either
+                               # a tuple with result, ie (1, ) or None
+
+        if id:
+            return id[0]
+        else:
+            cursor.execute('''
+                INSERT INTO merchants (name, category, risk_level)
+                VALUES (?, ?, ?)
+            ''', (merchant, category, risk_level))
+
+            conn.commit()
+
+            return cursor.lastrowid
+        
+
--- schema.sql
+++ schema.sql
@@ -1,23 +1,30 @@
+PRAGMA foreign_keys = ON; -- enforces foreign key constraints during this file
+                          -- execution (for insert statements defined)
 
-CREATE TABLE merchants IF NOT EXISTS( --only recreates table if it DNE
+CREATE TABLE IF NOT EXISTS merchants ( --only recreates table if it DNE
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    category VARCHAR(50) NOT NULL,
-    risk_level DECIMAL(3, 2) NOT NULL
+    name TEXT UNIQUE NOT NULL,
+    category TEXT NOT NULL,
+    risk_level REAL NOT NULL
 ) STRICT; 
 -- enforces types using STRICT
 
-CREATE TABLE transactions IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    amount DECIMAL(15, 2) NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    amount REAL NOT NULL,
     merchant_id INTEGER NOT NULL,
-    country VARCHAR(50) DEFAULT "USA",
-    rate DECIMAL(5, 4) DEFAULT 1.0000, 
-    status VARCHAR(20) NOT NULL,
+    country TEXT DEFAULT "USA",
+    rate REAL DEFAULT 1.0000, 
+    status TEXT NOT NULL,
     FOREIGN KEY (merchant_id) REFERENCES merchants(id) ON DELETE RESTRICT
 ) STRICT;
 
+-- Sample data
 INSERT INTO merchants(name, category, risk_level)
 VALUES ('Nike', 'Athletic', 0.05), ('Adidas', 'Athletic', 0.05), ('Hollister', 'Clothing', 0.10), 
 ('Shein', 'E-commerce', 0.40), ('Alibaba', 'E-commerce', 0.50);
+
+INSERT INTO transactions(amount, merchant_id, country, rate, status)
+VALUES (20.5, 2, 'United States', 1.0, 'Validated'), (6.70, 1, 'China', 2.78, 'Validated'),
+(4.20, 3, 'Bangladesh', 0.48, 'Validated'), (3.14, 5, 'Iran', 3.74, 'Blocked');
